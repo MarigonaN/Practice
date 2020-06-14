03e66f14dc1feec81dd3fc6cf25f8871cbe89075
MarigonaN <marigonanuhija@gmail.com>
2020-06-14 23:12:42 +0200

    Create BookList.jsx

---
+++ recap-react/src/components/BookList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import React, { Component } from 'react';
+
+class BookList extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                <h1>My Book Store</h1>
+            </div>
+        );
+    }
+}
+
+export default BookList;
