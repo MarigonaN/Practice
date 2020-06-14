1daed4932158b560a32e8a7109e1692293594514
MarigonaN <marigonanuhija@gmail.com>
2020-06-14 23:33:50 +0200

    Update BookList.jsx

--- recap-react/src/components/BookList.jsx
+++ recap-react/src/components/BookList.jsx
@@ -1,14 +1,31 @@
 
 import React, { Component } from 'react';
+import { Row } from 'react-bootstrap';
 
 class BookList extends Component {
-    state = {}
+    state = {
+        books: []
+    }
     render() {
-        return (
-            <div>
+        return (<>
+            <Row>
                 <h1>{this.props.text}</h1>
-            </div>
+            </Row>
+            <Row>
+            {this.state.books.map(book =>
+             <img src={book.img} style={{width: "200px"}} />)}
+            </Row>
+            </>
         );
+    }
+    componentDidMount = async () => {
+        const response = await fetch("https://striveschool.herokuapp.com/books?offset=0&limit=50")
+        if(response.ok){
+            const books = await response.json() 
+            this.setState({
+                books: books
+            })
+        }
     }
 }
 
