789890cb996f3989735c00f6679f475df2441f29
MarigonaN <marigonanuhija@gmail.com>
2020-07-10 04:33:18 +0200

    Update index.js

--- some-recap/src/services/users/index.js
+++ some-recap/src/services/users/index.js
@@ -5,28 +5,36 @@ const usersRouter = express.Router()
 
 usersRouter.get('/', async (req, res, next) => {
     try {
-        const usersList =await userSchema.find(req.query)
+        const usersList = await userSchema.find(req.query)
         res.send(usersList)
     } catch (error) {
         next(error)
     }
 })
+
+
+
+
 usersRouter.get('/:id', async (req, res, next) => {
     try {
-      const user =  await userSchema.findById(req.params.id)
-      if(user){
-        console.log(user)
-        res.send(user)
-      }else{
-          const error = new Error()
-          error.httpStatusCode = 404
-          next(error)
-      }
-      
+        const user = await userSchema.findById(req.params.id)
+        if (user) {
+            console.log(user)
+            res.send(user)
+        } else {
+            const error = new Error()
+            error.httpStatusCode = 404
+            next(error)
+        }
+
     } catch (error) {
         next(error)
     }
 })
+
+
+
+
 usersRouter.post('/', async (req, res, next) => {
     try {
         const newUser = new userSchema(req.body)
@@ -36,19 +44,27 @@ usersRouter.post('/', async (req, res, next) => {
         next(error)
     }
 })
+
+
+
+
 usersRouter.put('/:id', async (req, res, next) => {
     try {
-      const response =  await userSchema.findByIdAndUpdate(req.params.id, req.body)
+        const response = await userSchema.findByIdAndUpdate(req.params.id, req.body)
         console.log(response)
         res.send(response)
     } catch (error) {
         next(error)
     }
 })
+
+
+
+
 usersRouter.delete('/:id', async (req, res, next) => {
     try {
         const user = await userSchema.findByIdAndDelete(req.params.id)
-      
+
         res.send("deleted!")
     } catch (error) {
         next(error)
