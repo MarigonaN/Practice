732e0964209d769a7715f188fc9e89dce00a4ed9
MarigonaN <marigonanuhija@gmail.com>
2020-07-10 04:30:55 +0200

    Update index.js

--- some-recap/src/services/users/index.js
+++ some-recap/src/services/users/index.js
@@ -12,7 +12,20 @@ usersRouter.get('/', async (req, res, next) => {
     }
 })
 usersRouter.get('/:id', async (req, res, next) => {
-    
+    try {
+      const user =  await userSchema.findById(req.params.id)
+      if(user){
+        console.log(user)
+        res.send(user)
+      }else{
+          const error = new Error()
+          error.httpStatusCode = 404
+          next(error)
+      }
+      
+    } catch (error) {
+        next(error)
+    }
 })
 usersRouter.post('/', async (req, res, next) => {
     try {
@@ -23,7 +36,23 @@ usersRouter.post('/', async (req, res, next) => {
         next(error)
     }
 })
-usersRouter.put('/:id', async (req, res, next) => {})
-usersRouter.delete('/:id', async (req, res, next) => {})
+usersRouter.put('/:id', async (req, res, next) => {
+    try {
+      const response =  await userSchema.findByIdAndUpdate(req.params.id, req.body)
+        console.log(response)
+        res.send(response)
+    } catch (error) {
+        next(error)
+    }
+})
+usersRouter.delete('/:id', async (req, res, next) => {
+    try {
+        const user = await userSchema.findByIdAndDelete(req.params.id)
+      
+        res.send("deleted!")
+    } catch (error) {
+        next(error)
+    }
+})
 
 module.exports = usersRouter
